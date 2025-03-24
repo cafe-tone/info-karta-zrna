8da1b2d166ce968186caac4f5c17771210df0224
Vojta <vojtech.linha@gmail.com>
2025-03-24 21:27:10 +0100

    feat: working example

--- src/main.js
+++ src/main.js
@@ -13,25 +13,17 @@ document
       format: [6, 9], // 6cm x 9cm
     });
 
-    const source = document.getElementById("card");
-    doc.html(source, {
-      callback: function (doc) {
-        doc.save("info-zrna.pdf");
-      },
-      x: 0,
-      y: 0,
-    });
+    let y = 1;
+    doc.setFontSize(10);
+    doc.setFont("Roboto Slab", "bold");
+    for (const [key, value] of Object.entries(data)) {
+      doc.text(
+        `${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}`,
+        0.5,
+        y
+      );
+      y += 0.8;
+    }
 
-    // let y = 1;
-    // doc.setFontSize(10);
-    // for (const [key, value] of Object.entries(data)) {
-    //   doc.text(
-    //     `${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}`,
-    //     0.5,
-    //     y
-    //   );
-    //   y += 0.8;
-    // }
-
-    // doc.save(`${data.name || "info-zrna"}.pdf`);
+    doc.save(`${data.name || "info-zrna"}.pdf`);
   });
