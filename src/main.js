f3d4cc58982381f6882b8d4d8baa890830a4d57a
Vojta <vojtech.linha@gmail.com>
2025-03-24 20:59:38 +0100

    feat: tried jsPDF

--- src/main.js
+++ src/main.js
@@ -13,16 +13,25 @@ document
       format: [6, 9], // 6cm x 9cm
     });
 
-    let y = 1;
-    doc.setFontSize(10);
-    for (const [key, value] of Object.entries(data)) {
-      doc.text(
-        `${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}`,
-        0.5,
-        y
-      );
-      y += 0.8;
-    }
+    const source = document.getElementById("card");
+    doc.html(source, {
+      callback: function (doc) {
+        doc.save("info-zrna.pdf");
+      },
+      x: 0,
+      y: 0,
+    });
 
-    doc.save(`${data.name || "info-zrna"}.pdf`);
+    // let y = 1;
+    // doc.setFontSize(10);
+    // for (const [key, value] of Object.entries(data)) {
+    //   doc.text(
+    //     `${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}`,
+    //     0.5,
+    //     y
+    //   );
+    //   y += 0.8;
+    // }
+
+    // doc.save(`${data.name || "info-zrna"}.pdf`);
   });
